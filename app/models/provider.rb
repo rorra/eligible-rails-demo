f916e2102f46b531cf58123a1fb1d0a4f726fa7d
Rodrigo Dominguez <rorra@rorra.com.ar>
2013-09-17 16:12:37 -0300

    Typo on validator

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,7 +1,7 @@
 class Provider < ActiveRecord::Base
   belongs_to :user
 
-  validates :npi, presence: true, uniquenes: { scope: :user_id }
+  validates :npi, presence: true, uniqueness: { scope: :user_id }
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
